922864e93e7b4c6be90163e339fe28e7b171cc54
BaoGiaDev18 <baodmgse182883@fpt.edu.vn>
2025-02-03 19:26:23 +0700

    update animation player

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;  // Tốc độ di chuyển của nhân vật
+    private Animator animator;
+    private Vector2 movement;
+ 
+    void Start()
+    {
+        animator = GetComponent<Animator>();  // Lấy Animator của nhân vật
+    }
+
+    void Update()
+    {
+        // Lấy input từ người dùng để xác định hướng di chuyển
+       
+            movement.x = Input.GetAxisRaw("Horizontal");  // A/D hoặc phím mũi tên trái/phải
+            movement.y = Input.GetAxisRaw("Vertical");    // W/S hoặc phím mũi tên lên/xuống
+   
+        // Kiểm tra nếu có di chuyển thì bật trạng thái walking
+        if (movement != Vector2.zero)
+        {
+            animator.SetBool("isWalking", true);  // Chuyển sang trạng thái walking
+            animator.SetFloat("MoveX", movement.x);  // Xác định hướng đi trên trục X
+            animator.SetFloat("MoveY", movement.y);  // Xác định hướng đi trên trục Y
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);  // Nếu không di chuyển thì idle
+        }
+    }
+
+    void FixedUpdate()
+    {
+     transform.Translate(moveSpeed * Time.fixedDeltaTime * movement.normalized, Space.World);
+    }
+}
---
+++ Assets/Scripts/SortingOrderHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingOrderHandler : MonoBehaviour
+{
+    public GameObject player;
+    public int sortingOrderOffset = 0;  // Tùy chỉnh nếu bạn cần một giá trị offset
+    private SpriteRenderer spriteRenderer;
+    private Collider2D[] colliders;  // Mảng để chứa các Collider2D
+
+    void Start()
+    {
+        // Lấy component SpriteRenderer từ đối tượng mà script này gắn vào
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        player = GameObject.FindWithTag("Player");
+
+        // Lấy tất cả các Collider2D trên đối tượng này
+        colliders = GetComponents<Collider2D>();
+    }
+
+    void Update()
+    {
+        if (spriteRenderer != null && colliders.Length > 0)
+        {
+            // Tìm giá trị Y cao nhất từ các collider
+            float highestColliderTopY = float.MinValue;
+
+            foreach (Collider2D collider in colliders)
+            {
+                // Tìm giá trị Y của cạnh trên của từng collider
+                float colliderTopY = collider.bounds.max.y;
+
+                // Cập nhật giá trị Y cao nhất
+                if (colliderTopY > highestColliderTopY)
+                {
+                    highestColliderTopY = colliderTopY;
+                }
+            }
+
+            // So sánh vị trí Y của player với cạnh trên của collider cao nhất
+            if (player.transform.position.y < highestColliderTopY)
+            {
+                // Nếu nhân vật nằm phía dưới cạnh trên của collider, nhân vật sẽ nằm trước vật
+                spriteRenderer.sortingOrder = player.GetComponent<SpriteRenderer>().sortingOrder - 1 + sortingOrderOffset;
+            }
+            else
+            {
+                // Nếu nhân vật nằm phía trên cạnh trên của collider, nhân vật sẽ nằm sau vật
+                spriteRenderer.sortingOrder = player.GetComponent<SpriteRenderer>().sortingOrder + 1 + sortingOrderOffset;
+            }
+        }
+    }
+}
