8efa3c03379e40bd88149f73e97b6bf5cf7d6844
BaoGiaDev18 <baodmgse182883@fpt.edu.vn>
2025-03-07 14:00:49 +0700

    fix_time_stamina

--- Assets/Scripts/PersistentWorldTime.cs
+++ Assets/Scripts/PersistentWorldTime.cs
@@ -9,7 +9,6 @@ public class PersistentWorldTime : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
